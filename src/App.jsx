9759278fb4e4f788e9f9bbac329a29a41223f49b
Martin <97972556+VgHost-26@users.noreply.github.com>
2024-09-21 13:26:09 +0200

    css

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import CurrentWeather from "./Components/CurrentWeather"
 import ForecastWeather from "./Components/ForecastWeather"
 import HourWeather from "./Components/HourWeather"
 import Menu from "./Components/Menu"
+import Content from "./Components/Content"
 
 const apiUrl = "https://api.weatherapi.com/v1"
 const apiKey = "cad095b8ab0c448b8de112929241909"
@@ -16,8 +17,10 @@ const myCities = ["Gliwice", "Hamburg"]
 function App() {
   const [city, setCity] = useState(myCities[0])
   const [weatherData, setWeatherData] = useState("")
+  const [multipleWeatherData, setMultipleWeatherData] = useState([])
   const [unit, setUnit] = useState("c")
   const [isMenuHidden, setMenuIsHidden] = useState(true)
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth)
 
   const fetchWeather = () => {
     axios
@@ -36,6 +39,51 @@ function App() {
         console.error(error)
       })
   }
+  const fetchMultipleWeather = async () => {
+    // setMultipleWeatherData([])
+
+    try {
+      const promices = myCities.map(city =>
+        axios.get(
+          `${apiUrl}/forecast.json?key=${apiKey}&q=${city}&days=7&aqi=no&alerts=no`
+        )
+      )
+
+      const results = await Promise.all(promices)
+      // console.log(results);
+      setMultipleWeatherData(results)
+
+
+      // results.forEach(response => {
+      //   console.log("multiple", response.data)
+      //   setMultipleWeatherData([...multipleWeatherData, response.data])
+      // })
+    } catch (error) {
+      console.error(error)
+    }
+
+    // for (const ct of myCities) {
+    //   axios
+    //     .get(
+    //       `${apiUrl}/forecast.json?key=${apiKey}&q=${ct}&days=7&aqi=no&alerts=no`
+    //     )
+    //     .then(response => {
+    //       console.log("multi", response)
+    //       if (response.status === 200) {
+    //         setMultipleWeatherData([...multipleWeatherData, response.data])
+    //       } else {
+    //         console.error(response)
+    //       }
+    //     })
+    //     .catch(error => {
+    //       console.error(error)
+    //     })
+    // }
+  }
+  const handleWindowSizeChange = () => {
+    setWindowWidth(window.innerWidth)
+    console.log()
+  }
 
   const showHideMenu = () => {
     setMenuIsHidden(!isMenuHidden)
@@ -46,8 +94,16 @@ function App() {
   }
 
   useEffect(() => {
-    fetchWeather()
-    console.log(weatherData)
+    if (windowWidth > 800) {
+      fetchMultipleWeather()
+    } else {
+      fetchWeather()
+    }
+    // console.log(weatherData)
+    window.addEventListener("resize", handleWindowSizeChange)
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange)
+    }
   }, [city])
 
   useEffect(() => {
@@ -68,6 +124,7 @@ function App() {
         changeCity={changeCity}
         choosenCity={city}
       />
+
       <div id="header">
         <input
           type="button"
@@ -81,37 +138,34 @@ function App() {
         </a>
         <h1>NubiWeather</h1>
       </div>
-      <div className="content">
-        {weatherData ? <div className="title"><span>{weatherData.location.name}</span>, {weatherData.location.country}</div> : <></>}
-        {weatherData ? (
-          <CurrentWeather
-            data={weatherData}
-            unit={unit}
-            refresh={fetchWeather}
-          />
-        ) : (
-          <>{/*fetchWeather()*/}</>
-        )}
 
-        {weatherData ? (
-          <HourWeather
-            data={weatherData.forecast.forecastday[0].hour}
+      {windowWidth > 800 ? (
+        <>
+          {myCities.map((ct, i) => {
+            return (
+              <>
+                <div key={i} className="weather-list">
+                  <Content
+                    weatherData={
+                      multipleWeatherData ? (multipleWeatherData[i].data) : null
+                    }
+                    unit={unit}
+                    fetchWeather={fetchMultipleWeather}
+                  />
+                </div>
+              </>
+            )
+          })}
+        </>
+      ) : (
+        <>
+          <Content
+            weatherData={weatherData}
             unit={unit}
-            refresh={fetchWeather}
+            fetchWeather={fetchWeather}
           />
-        ) : (
-          <>{/*fetchWeather()*/}</>
-        )}
-
-        {weatherData ? (
-          <ForecastWeather
-            days={weatherData.forecast.forecastday}
-            unit={unit}
-          />
-        ) : (
-          <>{/*fetchWeather()*/}</>
-        )}
-      </div>
+        </>
+      )}
     </>
   )
 }
